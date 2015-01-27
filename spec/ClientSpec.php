97e92edfeab2adb9bed04187b7912a402879f1a0
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-01-27 12:21:03 +0100

    Initial implementation

---
+++ spec/ClientSpec.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace spec\Gravatar\Xmlrpc;
+
+use fXmlRpc\ClientInterface;
+use PhpSpec\ObjectBehavior;
+
+class ClientSpec extends ObjectBehavior
+{
+    function let(ClientInterface $client)
+    {
+        $this->beConstructedWith($client, 'secret_word');
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Gravatar\Xmlrpc\Client');
+    }
+
+    function it_checks_a_hash_existence(ClientInterface $client)
+    {
+        $hashes = ['hash1', 'hash2'];
+        $response = [
+            'hash1' => true,
+            'hash2' => false,
+        ];
+
+        $client->call('grav.exists', [
+            'hashes'   => $hashes,
+            'password' => 'secret_word',
+        ])->willReturn($response);
+
+        $this->exists($hashes)->shouldReturn($response);
+    }
+
+    function it_returns_addresses(ClientInterface $client)
+    {
+        $response = [
+            'user@domain.com' => [
+                'rating'        => 0,
+                'userimage'     => 1,
+                'userimage_url' => '?',
+            ],
+        ];
+
+        $client->call('grav.addresses', [
+            'password' => 'secret_word',
+        ])->willReturn($response);
+
+        $this->addresses()->shouldReturn($response);
+    }
+
+    function it_returns_user_images(ClientInterface $client)
+    {
+        $response = [
+            1 => [0, '?'],
+        ];
+
+        $client->call('grav.userimages', [
+            'password' => 'secret_word',
+        ])->willReturn($response);
+
+        $this->userimages()->shouldReturn($response);
+    }
+
+    function it_uploads_an_image(ClientInterface $client)
+    {
+        $data = base64_encode('image');
+        $rating = 0;
+        $response = 'userimage';
+
+        $client->call('grav.saveData', [
+            'data'     => $data,
+            'rating'   => $rating,
+            'password' => 'secret_word',
+        ])->willReturn($response);
+
+        $this->saveData($data, $rating)->shouldReturn($response);
+    }
+
+    function it_uploads_an_image_from_url(ClientInterface $client)
+    {
+        $url = 'url';
+        $rating = 0;
+        $response = 'userimage';
+
+        $client->call('grav.saveUrl', [
+            'url'      => $url,
+            'rating'   => $rating,
+            'password' => 'secret_word',
+        ])->willReturn($response);
+
+        $this->saveUrl($url, $rating)->shouldReturn($response);
+    }
+
+    function it_sets_the_used_image(ClientInterface $client)
+    {
+        $userimage = 'userimage';
+        $addresses = ['user@domain.com'];
+        $response = ['user@domain.com' => true];
+
+        $client->call('grav.useUserimage', [
+            'userimage' => $userimage,
+            'addresses' => $addresses,
+            'password'  => 'secret_word',
+        ])->willReturn($response);
+
+        $this->useUserimage($userimage, $addresses)->shouldReturn($response);
+    }
+
+    function it_removes_an_image(ClientInterface $client)
+    {
+        $addresses = ['user@domain.com'];
+        $response = ['user@domain.com' => true];
+
+        $client->call('grav.removeImage', [
+            'addresses' => $addresses,
+            'password'  => 'secret_word',
+        ])->willReturn($response);
+
+        $this->removeImage($addresses)->shouldReturn($response);
+    }
+
+    function it_deletes_an_image(ClientInterface $client)
+    {
+        $userimage = 'userimage';
+        $response = true;
+
+        $client->call('grav.deleteUserimage', [
+            'userimage' => $userimage,
+            'password'  => 'secret_word',
+        ])->willReturn($response);
+
+        $this->deleteUserimage($userimage)->shouldReturn($response);
+    }
+
+    function it_returns_test_data(ClientInterface $client)
+    {
+        $test = 'test';
+        $response = ['test' => $test];
+
+        $client->call('grav.test', [
+            'test'     => $test,
+            'password' => 'secret_word',
+        ])->willReturn($response);
+
+        $this->test($response)->shouldReturn($response);
+    }
+}
---
+++ src/Client.php
@@ -0,0 +1,176 @@
+<?php
+
+/*
+ * This file is part of the Gravatar XML-RPC API package.
+ *
+ * (c) Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gravatar\Xmlrpc;
+
+use fXmlRpc\ClientInterface;
+
+/**
+ * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ *
+ * @link http://en.gravatar.com/site/implement/xmlrpc/
+ */
+class Client
+{
+    /**
+     * @var ClientInterface
+     */
+    protected $xmlrpcClient;
+
+    /**
+     * @var string
+     */
+    protected $password;
+
+    /**
+     * @param ClientInterface $xmlrpcClient
+     * @param string          $password
+     */
+    public function __construct(ClientInterface $xmlrpcClient, $password)
+    {
+        $this->xmlrpcClient = $xmlrpcClient;
+        $this->password = $password;
+    }
+
+    /**
+     * Checks whether a hash has a gravatar
+     *
+     * @param array $hashes
+     *
+     * @return array
+     */
+    public function exists(array $hashes)
+    {
+        $params = compact('hashes');
+
+        return $this->call(__FUNCTION__, $params);
+    }
+
+    /**
+     * Returns a list of addresses for this account
+     *
+     * @return array
+     */
+    public function addresses()
+    {
+        return $this->call(__FUNCTION__);
+    }
+
+    /**
+     * Returns an array of userimages for this account
+     *
+     * @return array
+     */
+    public function userimages()
+    {
+        return $this->call(__FUNCTION__);
+    }
+
+    /**
+     * Save binary image data as a userimage for this account
+     *
+     * @param string  $data
+     * @param integer $rating
+     *
+     * @return boolean|string
+     */
+    public function saveData($data, $rating)
+    {
+        $params = compact('data', 'rating');
+
+        return $this->call(__FUNCTION__, $params);
+    }
+
+    /**
+     * Reads an image via its URL and save that as a userimage for this account
+     *
+     * @param string  $url
+     * @param integer $rating
+     *
+     * @return boolean|string
+     */
+    public function saveUrl($url, $rating)
+    {
+        $params = compact('url', 'rating');
+
+        return $this->call(__FUNCTION__, $params);
+    }
+
+    /**
+     * Use a userimage as a gravatar for one of more addresses on this account
+     *
+     * @param string $userimage
+     * @param array  $addresses
+     *
+     * @return array
+     */
+    public function useUserimage($userimage, array $addresses)
+    {
+        $params = compact('userimage', 'addresses');
+
+        return $this->call(__FUNCTION__, $params);
+    }
+
+    /**
+     * Removes the userimage associated with one or more email addresses
+     *
+     * @param array $addresses
+     *
+     * @return array
+     */
+    public function removeImage(array $addresses)
+    {
+        $params = compact('addresses');
+
+        return $this->call(__FUNCTION__, $params);
+    }
+
+    /**
+     * Removes a userimage from the account and any email addresses with which it is associated
+     *
+     * @param string $userimage
+     *
+     * @return boolean
+     */
+    public function deleteUserimage($userimage)
+    {
+        $params = compact('userimage');
+
+        return $this->call(__FUNCTION__, $params);
+    }
+
+    /**
+     * Test function
+     *
+     * @param array $params
+     *
+     * @return array
+     */
+    public function test(array $params)
+    {
+        return $this->call(__FUNCTION__, $params);
+    }
+
+    /**
+     * Invokes the XML-RPC Client
+     *
+     * @param string $method
+     * @param array  $params
+     *
+     * @return mixed
+     */
+    protected function call($method, array $params = [])
+    {
+        $params['password'] = $this->password;
+
+        return $this->xmlrpcClient->call('grav.'.$method, $params);
+    }
+}
